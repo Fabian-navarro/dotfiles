460341411f40e9798d1cf027f69973baed8d598f
Fabian-navarro <fabian.navarr.parraga@gmail.com>
2024-10-11 13:24:44 -0600

    Added nvim/

---
+++ .config/nvim/README.md
@@ -0,0 +1,10 @@
+**My personal config**
+
+Currently configured for autocompletion, autoformatting and linting in:
+- C++
+- Python
+- Rust
+
+# Credits
+
+1) Completely based on NvChad: https://github.com/NvChad/NvChad
---
+++ .config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,51 @@
+local plugins = {
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = {"python"},
+    event = "VeryLazy",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "clangd",
+        "clang-format",
+        "rust-analyzer",
+        "codelldb",
+        "pyright",
+        "mypy",
+        "ruff",
+        "black"
+      }
+    }
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function ()
+      return require "custom.configs.rust-tools"
+    end,
+    config = function (_, opts)
+      require('rust-tools').setup(opts)
+    end
+  },
+}
+return plugins
